3edc6cbbee33881e0031e96fba6d356cdcc87b47
Al Ivanoff <dfx29@Als-MacBook-Air.local>
2014-09-09 14:57:43 +0400

    Initial Commit

---
+++ compress/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  compress
+//
+//  Created by Al Ivanoff on 09/09/14.
+//  Copyright (c) 2014 Al Ivanoff. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
